33d95eadf09b47a530a0961cfccfe76567c2197e
khodbolep <khodbolepiyush7@gmail.com>
2025-05-06 16:57:49 +0530

    Update add_two_numbers.py

---
+++ add_two_numbers.py
@@ -0,0 +1,12 @@
+# add_two_numbers.py
+
+def add_two_numbers(a, b):
+    return a + b
+
+# Adding 10 and 15
+num1 = 10
+num2 = 15
+result = add_two_numbers(num1, num2)
+
+# Printing the result
+print(f"The sum of {num1} and {num2} is {result}")
