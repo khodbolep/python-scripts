bf192f2badb0ae2f422dceb6ec61992e69384a2c
khodbolep <khodbolepiyush7@gmail.com>
2025-05-07 13:59:08 +0530

    Update add_two_numbers.py

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,5 +1,9 @@
 import sys
 
+if len(sys.argv) != 3:
+    print("Usage: python3 add_two_numbers.py <num1> <num2>")
+    sys.exit(1)
+
 a = int(sys.argv[1])
 b = int(sys.argv[2])
-print("Sum:", a - b)
+print("Sum:", a + b)
