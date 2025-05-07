07cb9125d000e030425907ae674a6d4b6ec257ea
khodbolep <khodbolepiyush7@gmail.com>
2025-05-07 14:13:07 +0530

    Update add_two_numbers.py

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,9 +1,1 @@
-import sys
-
-if len(sys.argv) != 3:
-    print("Usage: python3 add_two_numbers.py <num1> <num2>")
-    sys.exit(1)
-
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-print("Sum:", a + b)
+print("this is a simple python program its version is v1")
